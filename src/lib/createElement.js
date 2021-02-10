9dbff577865a3bd50339fa5c495cad54c0f3d486
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-02-10 14:21:35 +0100

    Delete commit

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,7 +1,6 @@
 export default function createElement(type, props, ...children) {
-  // Sachen gesamelt
   const el = document.createElement(type)
   Object.assign(el, props)
-  children && el.append(...children.map(child => child.el ?? child)) // Sachen verteilt
+  children && el.append(...children.map(child => child.el ?? child))
   return el
 }
