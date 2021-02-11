cb0c20481f910401887a6a6ec8ad89924af305c4
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-02-11 12:53:56 +0100

    Add Buttons

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,23 +1,37 @@
 import './Navigation.css'
 import createElement from '../../lib/createElement'
 
-const buttonsConfig = [
-  { text: 'Slytherin' },
-  { text: 'Gryffindor' },
-  { text: 'Hufflepuff' },
-  { text: 'Ravenclaw' },
-]
-
 export default function Navigation(onNavigate) {
-  const buttons = buttonsConfig.map(({ text }) => {
-    const button = createElement(
-      'button',
-      { className: 'Navigation__button' },
-      text
-    )
-    button.addEventListener('click', () => onNavigate(text))
-    return button
-  })
+  const el = createElement('nav', { className: 'Navigation' })
 
-  return createElement('nav', { className: 'Navigation' }, ...buttons)
+  let currentSelection
+  let navigationItems
+
+  function setButtons(texts) {
+    const navigationButtons = texts.map(text => {
+      const button = createElement(
+        'button',
+        { className: 'Navigation__button' },
+        text
+      )
+      button.addEventListener('click', () => onNavigationItemClick(text))
+      return button
+    })
+    el.innerHTML = ''
+    el.append(...navigationButtons)
+    navigationItems = navigationButtons
+    currentSelection = navigationButtons[0]
+    currentSelection.className = 'active'
+  }
+
+  function onNavigationItemClick(text) {
+    currentSelection.classList.toggle('active')
+    currentSelection = navigationItems.filter(
+      item => item.textContent === text
+    )[0]
+    currentSelection.className = 'active'
+    onNavigate(text)
+  }
+
+  return { el, setButtons }
 }
